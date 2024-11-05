44ef20b8ed9cabba0a5ca9493e81deccc7850f3b
joganotigrinho <00001100125127sp@al.educacao.sp.gov.br>
2024-11-05 15:23:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'O que é cilindro ?',
+    ' E um objeto tridimencional delimirado pela superficie da translaçao completa de um segmento'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de marrocos ?',
+    'A capital do Marrocos '
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em inglẽs ?',
+    ' Y love you '
+)
